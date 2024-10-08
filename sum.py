01535409673a5966e76305399ea1f2e683c319ed
aryamandevv <asd9884@nyu.edu>
2024-10-08 16:49:52 -0400

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,6 @@
+def sum_integers(a, b):
+  return a+b
+
+num1 = int(input("Enter the first number: ")
+num2 = int(input("Enter the second number: ")
+print(f"The sum of {num1} and {num2} is {sum_integers(num1, num2)}.")
